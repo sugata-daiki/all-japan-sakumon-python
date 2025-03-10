df5f1e912a0827ed55a475e658c2da83789d8dff
sugata-daiki <sugata.daiki.58s@st.kyoto-u.ac.jp>
2025-03-10 17:44:30 +0900

    Use np.savetxt

--- chapter01/simpleOutput.py
+++ chapter01/simpleOutput.py
@@ -5,9 +5,12 @@ if __name__ == '__main__':
 
     fibonacci_seq = [1, 1]
     [fibonacci_seq.append(fibonacci_seq[-1] + fibonacci_seq[-2]) for _ in range(8)]
-    output_arr = [[i+1, fibonacci_seq[i]] for i in range(10)]
+    output_arr = np.array([[i+1, fibonacci_seq[i]] for i in range(10)], dtype=int)
 
+    np.savetxt("test.txt", output_arr, fmt='%s')
+    """
     with open("test.txt", "w") as f:
         f.writelines(" ".join(map(str, row)) + "\n" for row in output_arr)
+    """
     sys.exit(0)
 
